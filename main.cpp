db68308ae3edef6ea65bc45a7223f98890ab07b7
u1438871 <u1438871@gmail.com>
2026-09-23 18:11:47 +0000

    Merging exercise

--- main.cpp
+++ main.cpp
@@ -2,11 +2,7 @@
 
 int main() {
     std::cout << "Hello from Git!\n";
-<<<<<<< HEAD
     int i = 5;
-=======
-    int i = 10;
->>>>>>> 3e2656fd6cb68172160a8848785cb5028fd651eb
     int y = 2;
     int j = 10;
     return 0;
